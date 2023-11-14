b0c7824a52d7b5ea463037b9282a38bed895054e
Abdillamit <abdillamit08@gmail.com>
2023-11-14 15:23:09 +0600

    #31github.day.4

---
+++ #31github.day.4.ts
@@ -0,0 +1,13 @@
+// LeetCode
+
+// 2703. Return Length of Arguments Passed
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+const argumentsLength = (...args) => args.length;
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
